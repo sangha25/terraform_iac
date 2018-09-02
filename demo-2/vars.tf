eb2b1c6bfdf41c8c39652d801083866842c99c57
sangha25 <sanghapal25@gmail.com>
2018-09-02 08:22:56 +0100

    updated demo2/vars.tf with default ami region

--- demo-2/vars.tf
+++ demo-2/vars.tf
@@ -6,7 +6,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
+   	us-east-1 = "ami-13be557e"
+	us-west-2 = "ami-06b94666"
+	eu-west-1 = "ami-844e0bf7"
     
   }
 }
